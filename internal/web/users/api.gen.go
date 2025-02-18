6c34d1b59828bbb9de774bad4287cf84436f9980
PinchukovKK <kirillqwerty2101@gmail.com>
2025-02-18 20:10:03 +1000

    version 1.8.3.1

--- internal/web/users/api.gen.go
+++ internal/web/users/api.gen.go
@@ -16,8 +16,8 @@ import (
 
 // User defines model for User.
 type User struct {
-	Email    *string `json:"email,omitempty"`
 	Id       *uint   `json:"id,omitempty"`
+	Email    *string `json:"email,omitempty"`
 	Password *string `json:"password,omitempty"`
 }
 
